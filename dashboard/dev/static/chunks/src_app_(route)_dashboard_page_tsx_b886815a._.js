19b6790474ccbd1377f16fd97d56da7f7d0ffba1
vaibhav212303 <vaibhavp@testrig.co.in>
2026-05-23 18:39:24 +0530

    enhanced the version and updated the packages

--- dashboard/dev/static/chunks/src_app_(route)_dashboard_page_tsx_b886815a._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_d1ff1cd3._.js",
-  "static/chunks/node_modules_recharts_es6_d2b9927b._.js",
-  "static/chunks/node_modules_1e67431d._.js"
-],
-    source: "dynamic"
-});
